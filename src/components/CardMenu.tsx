c401652c71f302675ea1cd15439d235cddc230ce
nelifzyh <nelifauziyah07@gmail.com>
2025-10-16 09:40:42 +0700

    componen card menu

---
+++ src/components/CardMenu.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Image from "next/image";
+import { ChevronRight } from "lucide-react";
+
+interface CardMenuProps {
+    title: string;
+    imageSrc: string;
+}
+
+export function CardMenu({ title, imageSrc }: CardMenuProps) {
+    return (
+        <div className="bg-[#eef8ff] rounded-2xl shadow-md p-4 flex flex-col items-center hover:shadow-lg transition duration-300">
+            <div className="relative w-full h-40 rounded-lg overflow-hidden">
+                <Image
+                    src={imageSrc}
+                    alt={title}
+                    fill
+                    className="object-cover"
+                    quality={100}
+                />
+            </div>
+            <button className="mt-4 w-full bg-white text-blue-600 font-semibold text-sm py-2 rounded-full flex items-center justify-center relative border-2 border-gray-400">
+                <span>{title}</span>
+                <span className="absolute right-3 bg-gray-400 rounded-full p-1 flex items-center justify-center">
+                    <ChevronRight className="w-4 h-4 text-white" />
+                </span>
+            </button>
+        </div>
+    )
+}
