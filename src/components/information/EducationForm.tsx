4de22626fe69571c60588cf2d2a2e85dcf707cf3
nelifzyh <nelifauziyah07@gmail.com>
2025-10-16 15:59:11 +0700

    update text capital

--- src/components/information/EducationForm.tsx
+++ src/components/information/EducationForm.tsx
@@ -4,14 +4,14 @@ import ProfileSection from "./ProfileSection";
 export default function EducationForm() {
   return (
     <ProfileSection title="EDUCATION">
-      <InputField label="#1 Last Education" />
-      <InputField label="#1 Major of Last Education" />
-      <InputField label="#1 Institution Name of Last Education" />
-      <InputField label="#1 Institution City of Last Education" />
-      <InputField label="#2 Last Education" />
-      <InputField label="#2 Major of Last Education" />
-      <InputField label="#2 Institution Name of Last Education" />
-      <InputField label="#2 Institution City of Last Education" />
+      <InputField label="#1 LAST EDUCATION:" />
+      <InputField label="#1 MAJOR OF LAST EDUCATION:" />
+      <InputField label="#1 INSTITUTION NAME OF LAST EDUCATION:" />
+      <InputField label="#1 INSTITUTION CITY OF LAST EDUCATION:" />
+      <InputField label="#2 LAST EDUCATION:" />
+      <InputField label="#2 MAJOR OF LAST EDUCATION:" />
+      <InputField label="#2 INSTITUTION NAME OF LAST EDUCATION:" />
+      <InputField label="#2 INSTITUTION CITY OF LAST EDUCATION:" />
     </ProfileSection>
   );
 }
--- src/components/information/PersonalDataForm.tsx
+++ src/components/information/PersonalDataForm.tsx
@@ -9,15 +9,15 @@ export default function PersonalDataForm({ section }: PersonalDataFormProps) {
     if (section === "left") {
         return (
             <ProfileSection title="PERSONAL DATA">
-                <InputField label="Full Name" />
-                <InputField label="Nick Name" />
-                <InputField label="Mobile Number 1" />
-                <InputField label="Mobile Number 2 (if any)" />
-                <InputField label="Email Address" />
-                <InputField label="Citizenship" />
-                <InputField label="Country Born Place" />
-                <InputField label="City Born Place" />
-                <InputField label="Born Date" />
+                <InputField label="FULL NAME:" />
+                <InputField label="NICK NAME:" />
+                <InputField label="MOBILE NUMBER 1:" />
+                <InputField label="MOBILE NUMBER 2 (IF ANY):" />
+                <InputField label="EMAIL ADDRESS:" />
+                <InputField label="CITIZENSHIP:" />
+                <InputField label="COUNTRY BORN PLACE:" />
+                <InputField label="CITY BORN PLACE:" />
+                <InputField label="BORN DATE:" />
             </ProfileSection>
         );
     }
@@ -25,14 +25,14 @@ export default function PersonalDataForm({ section }: PersonalDataFormProps) {
     return (
         <div className="pt-10">
             <ProfileSection title=" ">
-                <InputField label="Ethnic Group" />
-                <InputField label="Gender" />
-                <InputField label="ID Card Number" />
-                <InputField label="Home Address" />
-                <InputField label="Domicile Address" />
-                <InputField label="Height (cm)" />
-                <InputField label="Weight (kg)" />
-                <InputField label="Blood Type" />
+                <InputField label="ETHNIC GROUP:" />
+                <InputField label="GENDER:" />
+                <InputField label="ID CARD NUMBER:" />
+                <InputField label="HOME ADDRESS:" />
+                <InputField label="DOMICILE ADDRESS:" />
+                <InputField label="HEIGHT (CM):" />
+                <InputField label="WEIGHT (KG):" />
+                <InputField label="BLOOD TYPE:" />
             </ProfileSection>
         </div>
     );
