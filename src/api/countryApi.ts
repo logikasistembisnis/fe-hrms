a2cb3445478a8cef7f21bcce25e02480fe98e726
nelifzyh <nelifauziyah07@gmail.com>
2025-10-24 12:43:07 +0700

    integrasi country

---
+++ src/api/countryApi.ts
@@ -0,0 +1,16 @@
+// src/api/countryApi.ts
+import axios from "axios";
+import { Country } from "./data";
+
+interface ApiResponse<T> {
+  data: T;
+  message?: string;
+  status?: string;
+}
+
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+
+export const getCountries = async (): Promise<ApiResponse<Country[]>> => {
+  const response = await axios.get<ApiResponse<Country[]>>(`${API_BASE}/country`);
+  return response.data;
+};
